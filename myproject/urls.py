4242e3af8d95253b73cc7c0b7d17314020311134
Pacharapon21 <aszxcvbnm1000@gamil.com>
2025-09-04 12:52:06 +0700

    home

--- myproject/urls.py
+++ myproject/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
+    path('home/', views.home_view, name='home'),
 ]
